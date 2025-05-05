523a10041d00d12f1f33a6fbd7ea707408743dd6
marcopriests <prtmrc98@gmail.com>
2025-05-05 17:21:22 +0200

    [UPD] Main done

---
+++ src/components/Main.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+
+const Main = () => {
+    return (
+        <main>
+            <h1>content goes here</h1>
+
+            <div className='cta'>
+                <div className='container'>
+                    <ul>
+                        <li>
+                            <a href="">
+                                <div className='cta-img'>
+                                    <img src="./public/img/buy-comics-digital-comics.png" alt="" />
+                                </div>
+                                DIGITAL COMICS
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <div className='cta-img'>
+                                    <img src="./public/img/buy-comics-merchandise.png" alt="" />
+                                </div>
+                                DC MERCHANDISES
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <div className='cta-img'>
+                                    <img src="./public/img/buy-comics-subscriptions.png" alt="" />
+                                </div>
+                                SUBSCRIPTION
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <div className='cta-img'>
+                                    <img src="./public/img/buy-comics-shop-locator.png" alt="" />
+                                </div>
+                                COMIC SHOP LOCATOR
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <div className='cta-img'>
+                                    <img src="./public/img/buy-dc-power-visa.svg" alt="img" />
+                                </div>
+                                DC POWER VISA
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default Main
