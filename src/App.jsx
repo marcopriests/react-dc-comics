5b6969dff015c01e7e6946113056de2aa800c91f
marcopriests <prtmrc98@gmail.com>
2025-05-05 16:41:56 +0200

    [UPD] Header done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import Header from "./components/Header"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Header />
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,43 @@
 const Header = () => {
-  return (
-    <figure>
-        <img src="dc-logo.png" alt="DC LOGO" />
-    </figure>
-  )
+    return (
+        <header className="container">
+
+            <img src="./public/img/dc-logo.png" alt="" />
+
+            <ul>
+                <li>
+                    <a href="">CHARACTERS</a>
+                </li>
+                <li>
+                    <a href="">COMICS</a>
+                </li>
+                <li>
+                    <a href="">MOVIES</a>
+                </li>
+                <li>
+                    <a href="">TV</a>
+                </li>
+                <li>
+                    <a href="">GAMES</a>
+                </li>
+                <li>
+                    <a href="">COLLECTIBLES</a>
+                </li>
+                <li>
+                    <a href="">VIDEOS</a>
+                </li>
+                <li>
+                    <a href="">FANS</a>
+                </li>
+                <li>
+                    <a href="">NEWS</a>
+                </li>
+                <li>
+                    <a href="">SHOP</a>
+                </li>
+            </ul>
+        </header>
+    )
 }
 
 export default Header
