1f4c51b2d24bb3769d779182030a2bd04b2d3470
marcopriests <prtmrc98@gmail.com>
2025-05-05 17:52:33 +0200

    [UPD] footer done

---
+++ src/components/Footer.jsx
@@ -0,0 +1,183 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="container">
+                <ul>
+                    <li>
+                        <h3 className='lis-title'>DC COMICS</h3>
+                    </li>
+                    <li>
+                        <a href="">
+                            Characters
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Comics
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Movies
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            TV
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Games
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Videos
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            News
+                        </a>
+                    </li>
+                    <li>
+                        <h3 className="list-title">SHOP</h3>
+                    </li>
+                    <li>
+                        <a href="">
+                            Shop DC
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Shop DC Collectibles
+                        </a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <h3 className='lis-title'>DC</h3>
+                    </li>
+                    <li>
+                        <a href="">
+                            Terms Of Use
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Privacy policy (New)
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Ad Choices
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Advertising
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Jobs
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Subscriptions
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Talent Workshops
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            CPSC Certificates
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Ratings
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Shop Help
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            Contact Us
+                        </a>
+                    </li>
+                </ul>
+                <ul>
+                    <li>
+                        <h3 className='lis-title'>SITES</h3>
+                    </li>
+                    <li>
+                        <a href="">
+                            DC
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            MAD Magazines
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            DC Kids
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            DC Universe
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            DC Power Visa
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div className="footer-cta">
+                <div className="container">
+                    <div>
+                        <a href="">SIGN-UP NOW!</a>
+                    </div>
+
+                    <div className="social">
+                        <h3>FOLLOW US</h3>
+
+                        <div className="social-icon">
+                            <img src="./public/img/footer-facebook.png" alt="" />
+                        </div>
+                        <div className="social-icon">
+                            <img src="./public/img/footer-twitter.png" alt="" />
+                        </div>
+                        <div className="social-icon">
+                            <img src="./public/img/footer-youtube.png" alt="" />
+                        </div>
+                        <div className="social-icon">
+                            <img src="./public/img/footer-pinterest.png" alt="" />
+                        </div>
+                        <div className="social-icon">
+                            <img src="./public/img/footer-periscope.png" alt="" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
